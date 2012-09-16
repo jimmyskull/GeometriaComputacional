bb40db34b8a8f1a6570d58279a55d18092777262
Paulo Roberto Urio <paulourio@gmail.com>
2012-09-16 17:00:17 -0300

    Convertido de GNU 99 para POSIC C 99
    Signed-off-by: Paulo Roberto Urio <paulourio@gmail.com>

--- benchmark.c
+++ benchmark.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
-#include <sys/resource.h>
 #include "benchmark.h"
 
 struct benchmark {
@@ -13,7 +12,7 @@ struct benchmark {
 static double get_time()
 {
     struct timeval t;
-    struct timezone tzp;
+    void *tzp;
 
     gettimeofday(&t, &tzp);
     return t.tv_sec + t.tv_usec * 1e-6;
